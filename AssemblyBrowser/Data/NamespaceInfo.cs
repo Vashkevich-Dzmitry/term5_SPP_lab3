21b427cf1ed21ab7bcc3d6b24d27efd046773981
dzmitry <dzmitry.vashkevich.a@gmail.com>
2022-12-04 15:15:34 +0300

    create data structure classes

--- AssemblyBrowser/Data/NamespaceInfo.cs
+++ AssemblyBrowser/Data/NamespaceInfo.cs
@@ -8,5 +8,12 @@ namespace AssemblyBrowser.Data
 {
     public class NamespaceInfo
     {
+        private Dictionary<string, ClassInfo> _classInfos;
+        public Dictionary<string, ClassInfo> ClassInfos { get { return _classInfos; } }
+
+        public NamespaceInfo()
+        {
+            _classInfos = new Dictionary<string, ClassInfo>();
+        }
     }
 }
