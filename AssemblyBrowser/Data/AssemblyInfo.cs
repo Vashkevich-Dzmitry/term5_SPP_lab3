374145f0a9c41cc4e922a808d78274c8a5cc01df
dzmitry <dzmitry.vashkevich.a@gmail.com>
2022-12-04 15:07:41 +0300

    Добавьте файлы проекта.

---
+++ AssemblyBrowser/Data/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace AssemblyBrowser.Data
+{
+    public class AssemblyInfo
+    {
+        private Dictionary<string, NamespaceInfo> _namespaceInfos;
+
+        public Dictionary<string, NamespaceInfo> NamespaceInfos { get { return _namespaceInfos; } }
+        public string Name { get; }
+        public string Path { get; }
+
+        internal AssemblyInfo(string name, string path)
+        {
+            _namespaceInfos = new Dictionary<string, NamespaceInfo>();
+            Name = name;
+            Path = path;
+        }
+    }
+}
