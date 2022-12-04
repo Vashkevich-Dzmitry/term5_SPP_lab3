774f25ff97531134dee30e6ba1176386328c80ca
dzmitry <dzmitry.vashkevich.a@gmail.com>
2022-12-04 17:45:38 +0300

    method info generator complited

--- AssemblyBrowser/MemberGenerators/MethodInfoGenerator.cs
+++ AssemblyBrowser/MemberGenerators/MethodInfoGenerator.cs
@@ -6,7 +6,33 @@ namespace AssemblyBrowser.MemberGenerators
     {
         MemberInfo IMemberInfoGenerator.GenerateMemberInfo(System.Reflection.MemberInfo memberInfo, string namespaceName, string className)
         {
-            throw new NotImplementedException();
+            if (memberInfo?.MemberType == System.Reflection.MemberTypes.Method)
+            {
+                System.Reflection.MethodInfo methodInfo = (memberInfo as System.Reflection.MethodInfo)!;
+                MethodInfo modelMethodInfo = new()
+                {
+                    ReturnType = methodInfo.ReturnType.Name,
+                    Name = methodInfo.Name,
+                    IsStatic = methodInfo.IsStatic
+                };
+
+                System.Reflection.ParameterInfo[] parameters = methodInfo.GetParameters();
+                string parametersInfo = "";
+                if (parameters.Length > 0)
+                {
+                    foreach (System.Reflection.ParameterInfo parameter in parameters)
+                    {
+                        parametersInfo += parameter.ParameterType.Name + " " + parameter.Name + ", ";
+                    }
+                    parametersInfo = parametersInfo[0..^2];
+                }
+                modelMethodInfo.ParametersInfo = parametersInfo;
+                return modelMethodInfo;
+            }
+            else
+            {
+                throw new ArgumentException("Something went wrong");
+            }
         }
     }
 }
